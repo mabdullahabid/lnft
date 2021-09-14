4f1d5e18749440ccaece47a1446916db6ff7d7cf
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-09-14 12:56:37 +0300

    #styleguide Replace primary button by StyleGuide Button and replace Market copy for Experiences

--- src/routes/market/index.svelte
+++ src/routes/market/index.svelte
@@ -3,6 +3,7 @@
   import { faSlidersH } from "@fortawesome/free-solid-svg-icons";
   import { artworks, show, user, results } from "$lib/store";
   import { info, err, goto } from "$lib/utils";
+  import Button from "$styleguide/components/Button";
   import Gallery from "$components/Gallery";
   import Results from "$components/Results";
   import Search from "$components/Search";
@@ -56,10 +57,12 @@
 
 <div
   class="container mx-auto flex flex-wrap flex-col-reverse md:flex-row sm:justify-between mt-10 md:mt-20">
-  <h2 class="md:mb-0">Market</h2>
+  <h2 class="md:mb-0">Experiences</h2>
 
   {#if $user && $user.is_artist}
-    <a href="/artwork/create" class="primary-btn">Submit a new artwork</a>
+    <a href="/artwork/create">
+      <Button primary>Submit a new experience</Button>
+    </a>
   {/if}
 </div>
 <div class="container mx-auto mt-10">
