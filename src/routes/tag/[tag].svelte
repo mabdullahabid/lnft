bb324aa58e2b557fd4073d91fd1e1f220d39dc78
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-09-24 17:26:41 +0300

    #search Fix layout for new cards when user do tag search

--- src/routes/tag/[tag].svelte
+++ src/routes/tag/[tag].svelte
@@ -2,7 +2,7 @@
   import { page } from "$app/stores";
   import { operationStore, subscription } from "@urql/svelte";
   import { getArtworksByTag } from "$queries/artworks";
-  import { Card } from "$comp";
+  import Card from "$styleguide/components/Card.svelte";
   import galleries from "$lib/galleries";
 
   let { tag } = $page.params;
@@ -16,7 +16,7 @@
 
   <div class="flex flex-wrap">
     {#each artworks as artwork (artwork.id)}
-      <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 pr-6 mb-10">
+      <div class="w-full md:w-full lg:w-1/2 xl:w-1/2 pr-6 mb-10">
         <Card {artwork} />
       </div>
     {/each}
