b3f30e4e4dc322108866654ddadee51589ce8f66
Adam Soltys <asoltys@gmail.com>
2021-10-26 20:27:34 -0700

    use env vars

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -12,20 +12,14 @@ import {
 import { goto as svelteGoto } from "$app/navigation";
 import { tick } from "svelte";
 
-let cad, btc, usd;
-
-/*
-btc = "5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225";
-cad = "1cedcd3f9122601a7e3ad4a3c5402256b4e7a5de7a057632e0dd03147372fb10";
-usd = "473788ca8362a542c73f11cfd3f92162457467e9e052b92ab8a2e794e0ccf228";
-*/
-btc = "6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d";
-cad = "0e99c1a6da379d1f4151fb9df90449d40d0608f6cb33a5bcbfc8c265f42bab0a";
-usd = "ce091c998b83c78bb71a632313ba3760f1763d9cfcffae02258ffa9865a37bd2";
+const btc = import.meta.env.VITE_BTC;
+const cad = import.meta.env.VITE_CAD;
+const usd = import.meta.env.VITE_USD;
 
 const fade = (n, o) => svelteFade(n, { ...o, duration: 50 });
 
 const publicPages = [
+  "password",
   "login",
   "register",
   "activate",
@@ -35,6 +29,14 @@ const publicPages = [
   "privacy-policy",
   "activate",
 ];
+
+const royaltyRecipientSystemType = 'system';
+const royaltyRecipientIndividualType = 'individual';
+
+const royaltyRecipientTypes = {
+  [royaltyRecipientSystemType] : "System",
+  [royaltyRecipientIndividualType] : "Individual"
+}
 
 const addressUser = (a) =>
   get(addresses) &&
@@ -120,10 +122,7 @@ const goto = (path) => {
   if (window) window.history.pushState(null, null, path);
 };
 
-const explorer =
-  import.meta && import.meta.env && import.meta.env !== "production"
-    ? import.meta.env.VITE_PUBLIC_EXPLORER
-    : "https://blockstream.info/liquid";
+const explorer = import.meta.env.VITE_EXPLORER
 
 const copy = (v) => {
   let textArea = document.createElement("textarea");
@@ -280,4 +279,7 @@ export {
   val,
   validateEmail,
   publicPages,
+  royaltyRecipientSystemType,
+  royaltyRecipientIndividualType,
+  royaltyRecipientTypes
 };
